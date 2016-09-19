31cb7aab966b902a25bf388e83ccd642c0fe0ad3
Muhinthan <m.tharmendran@gmail.com>
2016-09-19 22:13:51 +0000

    Finish database part

--- server.js
+++ server.js
@@ -1,118 +1,82 @@
 var express = require('express');
-var validUrl = require('valid-url');
 var mongo = require('mongodb').MongoClient;
+var Bing = require('node-bing-api')({ accKey: "" });
 
 var app = express();
 
-function generateCode() {
-    return Math.floor(1000 + Math.random() * 10000).toString();
-}
-
-function checkDB(collection, filter, callback) {
-    collection.find(filter, { original: 1, mini: 1, _id: 0 }).limit(1).toArray(function(err, documents) {
+function addSearch(str) {
+    mongo.connect('mongodb://localhost:27017/image-search', function(err, db) {
         if (err) throw err;
-    
-        // Searching for URL
-        if ('original' in filter) {  
-            if (documents.length) {
-                var entry = { original: documents[0].original, mini: documents[0].mini };
-                console.log('Entry found: ' + JSON.stringify(entry));
-                callback(entry);
-            } else {
-                console.log('Entry not found, creating new entry...');
-                callback( newEntry(collection, filter.original) );
-            }
-        } 
+        else { console.log('Connected to image-search'); }
         
-        // Searching for code
-        else if ('mini' in filter) { 
-            if (documents.length) {
-                console.log('Code found');
-                var originalURL = documents[0].original;
-                callback(originalURL);
-            } else {
-                console.log('Code not found');
-                callback({ error: 'Code not Found' });
-            }
-        } 
-        
-        else {
-            console.log('Should not be here...');
-        }
+        var pastSearch = db.collection('pastSearch');
+        var d = new Date();
+        var newTerm = { term: str, when: d.toISOString() };
+        pastSearch.insert(newTerm, function(err, doc) {
+            if (err) throw err;
+            
+            console.log('Added new search ' + JSON.stringify(newTerm));
+        });
     });
 }
 
-function newEntry(collection, url) {
-    var code = generateCode().toString();
-    var newLink = { original: url, mini: code };
-    
-    collection.insert(newLink, function(err, doc) {
+function getPast(callback) {
+    mongo.connect('mongodb://localhost:27017/image-search', function(err, db) {
         if (err) throw err;
-        console.log('Added new entry: ' + JSON.stringify(newLink));
+        else { console.log('Connected to image-search'); }
+        
+        var pastSearch = db.collection('pastSearch');
+        
+        var searchList = [];
+        
+        pastSearch.find({}, { term: 1, when: 1, _id: 0 }).toArray(function(err, doc) {
+            if (err) throw err;
+            
+            for (var i=0; i < doc.length; i++) {
+                searchList.push({ term: doc[i].term, when: doc[i].when });
+            }
+            callback(searchList);
+        });
     });
-    return newLink;
 }
-
-
 
 app.use('/', express.static(__dirname + '/styles'));
-app.use('/new', express.static(__dirname + '/styles'));
 
-app.get('/new/:url(*)', function(req, res){
+app.get('/api/imagesearch/:terms', function(req, response){
     
-    var inputURL = req.params.url;
+    //var terms = req.params.terms.split('?')[0];
+    //var offset = req.params.terms.split('?')[1];
     
-    if (!validUrl.isUri(inputURL)){
-        res.json({error: 'Invalid url format, see main page for correct format'});
-    } else {
+    var term ="Ninja Turtles";
+    
+    Bing.images(term, {top: 5}, function(err, res, body){
+        if (err) throw err;
         
-        mongo.connect('mongodb://localhost:27017/url-shortener', function(err, db) {
-            if (err) throw err;
-            else { console.log('Connected to url-shortener.'); }
-            
-            var linksDB = db.collection('links');
-            
-            
-            // Checking if entry already exists
-            console.log('Beginning search');
-            checkDB(linksDB, { original: inputURL }, function(message) {
-                res.json(message);
-                db.close();
+        var images = body.d.results;
+
+        var printStuff = [];
+        
+        for (var i=0; i < images.length; i++) {
+            printStuff.push({ 
+                url: images[i].MediaUrl, 
+                snippet: images[i].Title,
+                thumbnail : images[0].Thumbnail.MediaUrl,
+                context: images[0].SourceUrl
             });
-        });
-    }
+        }
+        //addSearch(term);
+        response.send(printStuff);
+    });
+    
 });
 
 
-app.get('/:code(*)', function(req, res){
+app.get('/api/latest/imagesearch', function(req, res){
     
-    var code = req.params.code;
+    getPast(function(searchList) {
+        res.send(searchList);
+    });
     
-    if (!(code.match(/^\d+$/))){
-        res.json({error: 'Invalid code format, see main page for correct format'});
-    } else {
-        
-        mongo.connect('mongodb://localhost:27017/url-shortener', function(err, db) {
-            if (err) throw err;
-            else { console.log('Connected to url-shortener.'); }
-            
-            var linksDB = db.collection('links');
-            
-            
-            // Checking if entry already exists
-            console.log('Beginning search');
-            checkDB(linksDB, { mini: code }, function(message) {
-                if (typeof message === 'string') {
-                    console.log('Redirecting to ' + message);
-                    res.redirect(message);
-                } else {
-                    res.json(message);
-                }
-                
-                db.close();
-            });
-        });
-    }
 });
 
 
